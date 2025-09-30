74079ee2a3e358ea1be45db3bd0491be60db44fa
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-09-30 07:34:55 +0500

    added

--- src/lib/blockchain/topBlockchains.ts
+++ src/lib/blockchain/topBlockchains.ts
@@ -4,25 +4,25 @@
  */
 export const TOP_BLOCKCHAINS = [
   'ethereum',        // Ethereum
-  'bitcoin',         // Bitcoin
+  // 'bitcoin',         // Bitcoin
   'binance-smart-chain', // Binance Smart Chain (BSC)
   'solana',          // Solana
   'polygon-pos',     // Polygon
   'avalanche',       // Avalanche
   'arbitrum-one',    // Arbitrum
-  'optimistic-ethereum', // Optimism
+  // 'optimistic-ethereum', // Optimism
   'tron',            // Tron
   'base',            // Base
-  'cardano',         // Cardano
-  'cosmos',          // Cosmos
-  'polkadot',        // Polkadot
-  'near-protocol',   // Near
+  // 'cardano',         // Cardano
+  // 'cosmos',          // Cosmos
+  // 'polkadot',        // Polkadot
+  // 'near-protocol',   // Near
   'fantom',          // Fantom
-  'xai',             // XAI
-  'sei-v2',          // Sei Network
-  'internet-computer', // Internet Computer
-  'ronin',           // Ronin
-  'celestia'         // Celestia
+  // 'xai',             // XAI
+  // 'sei-v2',          // Sei Network
+  // 'internet-computer', // Internet Computer
+  // 'ronin',           // Ronin
+  // 'celestia'         // Celestia
 ];
 
 /**
