8c2597f2972705dcbd6588bb330cb1c4c609d16a
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-10-11 18:57:25 +0500

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
@@ -25,4 +25,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
