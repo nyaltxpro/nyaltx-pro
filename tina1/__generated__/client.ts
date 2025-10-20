b5cbd2146d952896ef4fb2e565dfcc99aba7f2fc
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-10-20 08:52:39 +0500

    added

--- tina1/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/mac/CascadeProjects/2048/tina/__generated__/.cache/1760929861034', url: 'https://content.tinajs.io/1.6/content/875ea65f-9159-4773-857e-d07f66271e14/github/main', token: '744dcb2882cb9acd09e800e9b06b21792965972d', queries,  });
-export default client;
-  
