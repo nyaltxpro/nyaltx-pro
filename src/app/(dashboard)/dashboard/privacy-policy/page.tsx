14cd6d093492f2811aab548de5f3d543249591e4
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-10-23 02:57:48 +0500

    added

--- src/app/(dashboard)/dashboard/privacy-policy/page.tsx
+++ src/app/(dashboard)/dashboard/privacy-policy/page.tsx
@@ -1,5 +1,12 @@
+import { tinaClient } from '@/lib/tinaClient';
 import PrivacyPolicy from '@/page-components/PrivacyPolicy';
 
-export default function PrivacyPolicyPage() {
-  return <PrivacyPolicy />;
-}
+const DashboardPrivacyPolicyPage = async () => {
+  const tinaData = await tinaClient.queries.privacyPolicy({
+    relativePath: 'privacypolicy.json',
+  });
+
+  return <PrivacyPolicy tinaData={tinaData} />;
+};
+
+export default DashboardPrivacyPolicyPage;
