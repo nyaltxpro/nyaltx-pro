c58e734479123e6666c23cef6051b9dc5f1d73a0
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-10-23 03:17:28 +0500

    added

--- src/page-components/PrivacyPolicy.tsx
+++ src/page-components/PrivacyPolicy.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import Header from '@/components/Header';
 import { useTina } from 'tinacms/dist/react';
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
+import PublicHeader from '../components/PublicHeader';
 
 interface PrivacyPolicyProps {
     tinaData: any;
@@ -15,23 +15,23 @@ const PrivacyPolicy: React.FC<PrivacyPolicyProps> = ({ tinaData }) => {
     const hero = pageData.hero;
     const heroStyle = hero?.backgroundImage
         ? {
-              backgroundImage: `url(${hero.backgroundImage})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-          }
+            backgroundImage: `url(${hero.backgroundImage})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }
         : undefined;
 
     return (
         <div>
-            <Header />
+            <PublicHeader />
             {hero ? (
                 <section className="hero-section p-12 text-center text-white" style={heroStyle}>
-                    <h1 className="text-4xl font-bold">{hero.title}</h1>
+                    <h1 className="text-4xl text-cyan-500   font-bold">{hero.title}</h1>
                     <p className="mt-4 text-lg">{hero.subtitle}</p>
                 </section>
             ) : null}
 
-            <section className="content-section max-w-4xl mx-auto p-8">
+            <section className="content-section max-w-5xl mx-auto p-8">
                 <TinaMarkdown content={pageData.content} />
             </section>
         </div>
