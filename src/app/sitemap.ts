5f5993b045aa0fabf851770d380c0afc92fe95db
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-10-31 07:52:31 +0500

    added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl =  'https://nyaltx.pro';
+  const baseUrl =  'https://www.nyaltx.pro';
 
   return [
     // Main Pages
