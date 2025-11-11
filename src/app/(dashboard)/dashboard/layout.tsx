676d22cffa40d677439587a11f2d557ce207b36b
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-11-11 13:16:06 +0500

    Added

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,31 +1,10 @@
 
 "use client";
 
-import { Inter, Poppins, Roboto } from "next/font/google";
 import { useState } from 'react';
 
-import Header from "@/components/HeaderUpdated"; // Assuming HeaderUpdated is the one to use
+import Header from "@/components/HeaderUpdated";
 import Sidebar from "@/components/Sidebar";
-
-
-
-// Removed duplicate web3modal import - already imported in providers
-
-const poppins = Poppins({
-    weight: ['400', '500', '600', '700'],
-    subsets: ["latin"],
-    variable: "--font-poppins",
-});
-
-const inter = Inter({ subsets: ['latin'] })
-
-const roboto = Roboto({
-    weight: ['400', '500', '700'],
-    subsets: ['latin'],
-    variable: '--font-roboto',
-});
-
-
 
 export default function RootLayout({
     children,
@@ -39,20 +18,16 @@ export default function RootLayout({
     };
 
     return (
-
-        <body className={inter.className}>
-
+        <div className="transition-all duration-300 flex min-h-screen bg-transparent">
             <Sidebar isMobileMenuOpen={isMobileMenuOpen} toggleMobileMenu={toggleMobileMenu} />
             {/* Apply margin-left for desktop, none for mobile */}
-            <div className="md:ml-16 transition-all duration-300 flex flex-col min-h-screen">
+            <div className="flex flex-1 flex-col md:ml-16">
                 <Header toggleMobileMenu={toggleMobileMenu} />
-                <main className="flex-grow">
+                <main className="grow">
                     {children}
                 </main>
                 {/* <Footer /> */}
             </div>
-
-        </body>
-
+        </div>
     );
 }
